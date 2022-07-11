e0fcf09f1a376f08030d5e11f9589ab289c63af7
acomas <acomas@PC-GSP234>
2022-07-11 13:19:37 +0200

    Agregar archivos de proyecto.

---
+++ testTDD/Src/Infrastructure/Form1.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.cmbEmpreses = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.txtSubs = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Location = New System.Drawing.Point(40, 181)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(94, 29)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Añadir"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'cmbEmpreses
+        '
+        Me.cmbEmpreses.FormattingEnabled = True
+        Me.cmbEmpreses.Location = New System.Drawing.Point(145, 51)
+        Me.cmbEmpreses.Name = "cmbEmpreses"
+        Me.cmbEmpreses.Size = New System.Drawing.Size(151, 28)
+        Me.cmbEmpreses.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(40, 54)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(66, 20)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Empresa"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(40, 97)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(99, 20)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Subscripcions"
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Button2.Location = New System.Drawing.Point(145, 181)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(94, 29)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Consultar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'txtSubs
+        '
+        Me.txtSubs.Location = New System.Drawing.Point(145, 94)
+        Me.txtSubs.Name = "txtSubs"
+        Me.txtSubs.Size = New System.Drawing.Size(125, 27)
+        Me.txtSubs.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(336, 242)
+        Me.Controls.Add(Me.txtSubs)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cmbEmpreses)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents cmbEmpreses As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents txtSubs As TextBox
+End Class
---
+++ testTDD/Src/Infrastructure/Form1.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Data.Sql
+Imports System.Data.SqlClient
+Public Class Form1
+
+    Private _conn
+    Sub New()
+        ' Esta llamada es exigida por el diseñador.
+        InitializeComponent()
+
+        ' Agregue cualquier inicialización después de la llamada a InitializeComponent().
+        _conn = New DBConnection("data source=localhost;initial catalog=testCodely;persist security info=True; Integrated Security=SSPI;")
+        OnLoad()
+    End Sub
+
+    Private Sub OnLoad()
+        emplenarComboEmpreses()
+
+    End Sub
+
+    Private Sub emplenarComboEmpreses()
+        Dim query = "SELECT Name FROM Empreses"
+        Dim ds As DataTable = _conn.doQuery(query)
+        cmbEmpreses.DataSource = ds
+        cmbEmpreses.DisplayMember = "Name"
+        cmbEmpreses.ValueMember = "Name"
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim empresa = cmbEmpreses.SelectedValue.ToString
+        Dim quantitat = txtSubs.Text
+        addSubscription(empresa, quantitat)
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbEmpreses.SelectedIndexChanged
+
+    End Sub
+End Class
